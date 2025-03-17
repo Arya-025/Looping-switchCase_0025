afaff68caf82598f0b85e93bfde59d4d88b66f32
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-17 11:32:42 +0700

    perulangan for  pencacah naik dan turun

--- contoh_Lopping.cpp
+++ contoh_Lopping.cpp
@@ -7,5 +7,16 @@ int main ()
     int perulanganWhile = 0;
     int perulanganDo =0;
     
- 
+    // perulangan dengan for
+
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "nilai n = " << n << "Selamat datang" << endl;
+    }
+    cout << "nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "perulangan for pencacah turun" << endl;
+  
 }
