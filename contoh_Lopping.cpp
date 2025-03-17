96612445a984e8e5a8710a0531a57ba214ebb6bf
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-17 11:26:21 +0700

    perulangan dengan for

--- contoh_Lopping.cpp
+++ contoh_Lopping.cpp
@@ -6,5 +6,27 @@ int main ()
     int n;
     int perulanganWhile = 0;
     int perulanganDo =0;
+    
+    // perulangan dengan for
 
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "nilai n = " << n << "Selamat datang" << endl;
+    }
+    cout << "nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "perulangan for pencacah turun" << endl;
+    // perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "nilai n + " << "Selamat datang" << endl;
+    }
+    cout << "nilai n terakhir = " << n;
+    cout << endl;
+
+    srand(time(0));
+
+    cout << "nilai awal n = " << n << endl;
 }
